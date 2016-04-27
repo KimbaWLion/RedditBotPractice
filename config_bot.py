f0a27a88101862d734b4b3666199b80d19620de5
TODO <merubin14@gmail.com>
2016-04-27 15:26:26 -0400

    Update config_bot.py

--- config_bot.py
+++ config_bot.py
@@ -3,5 +3,5 @@
 
 # Fill in the values and rename this file to config_bot.py
 
-REDDIT_USERNAME = 'test'  # YOUR USERNAME as string
-REDDIT_PASS = 'test'  # YOUR PASSWORD as string
+REDDIT_USERNAME = ''  # YOUR USERNAME as string
+REDDIT_PASS = ''  # YOUR PASSWORD as string
